d42030183cb5f2522ad000afd170e4ea2c68b2a0
Jason Simpson <jsimpson@concord.org>
2017-08-25 22:18:17 -0700

    Mount image for qemu configuration.

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@
 GENERATED_DIRS="kernels images"
 
 if [ "$1" == "clean" ]; then
-    echo "Cleaning workspace... "
+    echo "Cleaning workspace..."
     for DIR in $GENERATED_DIRS; do
         rm -rf $DIR
     done
@@ -22,6 +22,7 @@ done
 #
 # Download kernels
 #
+echo "Downloading kernels..."
 cd kernels
 if [ ! -d "qemu-rpi-kernel" ]; then
     git clone https://github.com/dhruvvyas90/qemu-rpi-kernel.git
@@ -29,9 +30,28 @@ fi
 cd ..
 
 #
-# Download image(s)
+# Download and unzip image
 #
+echo "Downloading image $IMAGE..."
 cd images
-    wget -N $IMAGE_URL
+wget -N $IMAGE_URL
+if [ ! -f "$IMAGE" ]; then
+    echo "Unzipping image $IMAGE..."
+    unzip -o $IMAGE_ZIP
+fi
 cd ..
+
+#
+# Mount image and modify for qemu
+#
+echo "Configuring image $IMAGE..."
+sudo mkdir -p /mnt/rpi
+sudo kpartx -av images/$IMAGE
+sleep 5
+sudo mount /dev/mapper/loop0p2 /mnt/rpi
+
+sudo umount /mnt/rpi
+sudo kpartx -d images/$IMAGE
+sudo rmdir /mnt/rpi
+
 
--- env.sh
+++ env.sh
@@ -22,3 +22,11 @@
 KERNEL=kernel-qemu-4.4.12-jessie
 IMAGE_URL=http://downloads.raspberrypi.org/raspbian/images/raspbian-2016-05-31/2016-05-27-raspbian-jessie.zip
 
+
+#
+# Set IMAGE_ vars based on IMAGE_URL
+#
+IMAGE_ZIP=`echo $IMAGE_URL | sed 's/.*\///'`
+IMAGE=`echo $IMAGE_ZIP | sed 's/\.zip$/.img/'`
+
+
